820e90192209824eadf4dc565f72182bcf2377d1
Benton Edmondson <bentonedmondson@gmail.com>
2020-04-08 21:47:58 -0500

    estimator is in pretty good shape

--- src/app/estimator/estimator.component.ts
+++ src/app/estimator/estimator.component.ts
@@ -9,16 +9,19 @@ import * as moment from 'moment';
 })
 export class EstimatorComponent implements OnInit {
 
-  buttonTimes: Array<number> = Array(Number(this.route.snapshot.paramMap.get('numTasks')));
-  eta: number = undefined;
+  startTime: number = moment().valueOf();
+  buttons: Array<number> = Array(Number(this.route.snapshot.paramMap.get('numTasks'))).fill(0).map((x, i) => i);
+  curTask: number = 1;
+  etoc: string = 'n/a';
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
-  pressButton = index => {
-    console.log(`${index} was pressed!`);
+  finishTask = (task) => {
+    this.etoc = moment(((this.buttons.length / task) * (moment().valueOf() - this.startTime) + this.startTime)).local().format("h:mm A");
+    this.curTask = task + 1;
   }
 
 }
