05ad5ca099217298f48a8e62b96199ca3196fd87
Benton Edmondson <bentonedmondson@gmail.com>
2020-04-18 15:22:24 -0500

    user can now take breaks, and these are factored into the etoc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { EstimatorComponent } from './estimator/estimator.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/estimator/estimator.component.ts
+++ src/app/estimator/estimator.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import * as moment from 'moment';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-estimator',
@@ -13,19 +14,48 @@ export class EstimatorComponent implements OnInit {
   timeSinceStart: string = '0:00:00';
   buttons: Array<number> = Array(Number(this.route.snapshot.paramMap.get('numTasks'))).fill(0).map((x, i) => i);
   curTask: number = 1;
-  etoc: string = 'n/a';
+  etoc;
+  totalQuantityOfBreaks: number = 0;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
     setInterval(() => {
       this.timeSinceStart = moment.utc(moment().diff(this.startTime)).format('H:mm:ss');
-    }, 500)
+    }, 500);
   }
 
   finishTask = (task) => {
-    this.etoc = moment(((this.buttons.length / task) * (moment().valueOf() - this.startTime.valueOf()) + this.startTime.valueOf())).local().format('h:mm A');
+    this.etoc = moment(
+      (this.buttons.length / task) * (moment().valueOf() - this.startTime.valueOf() - this.totalQuantityOfBreaks) + this.startTime.valueOf() + this.totalQuantityOfBreaks
+    );
     this.curTask = task + 1;
   }
+
+  dialogBox;
+  takeBreak = (templateRef) => {
+    let initialBreakQuantity = this.totalQuantityOfBreaks;
+    let initialEtoc = this.etoc
+    let breakStartTime = moment().valueOf();
+    this.dialogBox = this.dialog.open(templateRef, {
+      width: '250px'
+    });
+
+    let intervalRef = setInterval(() => {
+      this.totalQuantityOfBreaks = initialBreakQuantity + (moment().valueOf() - breakStartTime);
+      console.log(this.totalQuantityOfBreaks);
+      if (this.curTask > 1) this.etoc = moment(initialEtoc.valueOf() + this.totalQuantityOfBreaks);
+    }, 500);
+
+    this.dialogBox.afterClosed().subscribe(() => {
+      clearInterval(intervalRef);
+      this.totalQuantityOfBreaks = initialBreakQuantity + (moment().valueOf() - breakStartTime);
+    });
+  }
+
+  closeDialogBox = () => this.dialogBox.close();
 
 }
