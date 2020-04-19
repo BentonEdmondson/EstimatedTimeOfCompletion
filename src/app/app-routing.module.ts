5e197b73d02f3a0f9a48a448e0f5bd71519e3f84
BentonEdmondson <55590329+BentonEdmondson@users.noreply.github.com>
2020-04-18 22:04:58 -0500

    Initial branch (#1)
    * initial commit
    
    * estimator is in pretty good shape
    
    * buttons now have a margin surrounding them
    
    * text sizes change according to screen size
    
    * running timer now exists in the top middle of the screen
    
    * background has been updated
    
    * user can now take breaks, and these are factored into the etoc
    
    * cleaned up the button and timer css
    
    * user is congratulated upon completing all their tasks
    
    * the theming/background is now working!
    
    * homepage is now working
    
    * added instructions for how to use the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { EstimatorComponent } from './estimator/estimator.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'app/:numTasks', component: EstimatorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/estimator/estimator.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import * as moment from 'moment';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-estimator',
+  templateUrl: './estimator.component.html',
+  styleUrls: ['./estimator.component.css']
+})
+export class EstimatorComponent implements OnInit {
+
+  startTime = moment();
+  timeSinceStart: string = '0:00:00';
+  buttons: Array<number> = Array(Number(this.route.snapshot.paramMap.get('numTasks'))).fill(0).map((x, i) => i);
+  curTask: number = 1;
+  etoc;
+  totalQuantityOfBreaks: number = 0;
+
+  constructor(
+    private route: ActivatedRoute,
+    private dialog: MatDialog,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.timeSinceStart = moment.utc(moment().diff(this.startTime)).format('H:mm:ss');
+    }, 500);
+  }
+
+  finishTask = task => {
+    this.etoc = moment(
+      (this.buttons.length / task) * (moment().valueOf() - this.startTime.valueOf() - this.totalQuantityOfBreaks) + this.startTime.valueOf() + this.totalQuantityOfBreaks
+    );
+    this.curTask = task + 1;
+  }
+
+  dialogBox;
+  takeBreak = templateRef => {
+    let initialBreakQuantity = this.totalQuantityOfBreaks;
+    let initialEtoc = this.etoc
+    let breakStartTime = moment().valueOf();
+    this.dialogBox = this.dialog.open(templateRef, {});
+
+    let intervalRef = setInterval(() => {
+      this.totalQuantityOfBreaks = initialBreakQuantity + (moment().valueOf() - breakStartTime);
+      if (this.curTask > 1) this.etoc = moment(initialEtoc.valueOf() + this.totalQuantityOfBreaks);
+    }, 500);
+
+    this.dialogBox.afterClosed().subscribe(() => {
+      clearInterval(intervalRef);
+      this.totalQuantityOfBreaks = initialBreakQuantity + (moment().valueOf() - breakStartTime);
+    });
+  }
+
+  closeDialogBox = () => this.dialogBox.close();
+
+  congratulateUser = templateRef => this.dialogBox = this.dialog.open(templateRef, {});
+
+  returnHome = () => {
+    this.dialogBox.close();
+    this.router.navigate(['']);
+  }
+
+}
