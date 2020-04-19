080e83ddf766af32878f846a6b942529242f02b1
Benton Edmondson <bentonedmondson@gmail.com>
2020-04-18 21:33:07 -0500

    homepage is now working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 import { EstimatorComponent } from './estimator/estimator.component';
 
 
 const routes: Routes = [
+  { path: '', component: HomepageComponent },
   { path: 'app/:numTasks', component: EstimatorComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,24 @@ import { EstimatorComponent } from './estimator/estimator.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EstimatorComponent
+    EstimatorComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
